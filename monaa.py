b32038156a469ee97524e25801b7adf42d1d0fdf
AndersToft20 <124463248+aatBct@users.noreply.github.com>
2024-03-08 10:55:15 +0100

    return result

--- monaa.py
+++ monaa.py
@@ -4,21 +4,12 @@ def monaa_handler(file, regex: str):
     path = os.path.join("./temp_files/monaa_input", "log.txt")
     file.save(path)
 
-    
-    
     #call mona with input however that may be done
     command = f"../monaa/build/monaa -e '{regex}' < ./temp_files/monaa_input/log.txt > ./temp_files/monaa_output/monaa_output.txt"
+    os.system(command)
 
-    result = os.system(command)
 
-    #return monaa result
-    return """
-        0.300000       <= t <   0.500000
-        0.700000        < t' <   1.500000
-        0.200000        < t' - t <   1.000000
-        =============================
-        0.300000       <= t <   0.500000
-        0.700000        < t' <   1.500000
-        0.200000        < t' - t <   1.000000
-        =============================
-    """
+    with open("./temp_files/monaa_output/monaa_output.txt", "r") as f:
+        data = f.read()
+        return data
+
