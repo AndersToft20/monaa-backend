6d3fc543090f0babdcb2a5eecf00096392f22d3a
AndersToft20 <124463248+aatBct@users.noreply.github.com>
2024-03-08 10:40:31 +0100

    subprocess

--- monaa.py
+++ monaa.py
@@ -1,13 +1,23 @@
 import os
+import subprocess
 
 def monaa_handler(file, regex: str):
     print(file)
-    path = os.path.join("temp_files", "log.txt")
+    path = os.path.join("temp_files\monaa_input", "log.txt")
     file.save(path)
 
     
     
     #call mona with input however that may be done
+    command = f"../monaa/build/monaa -e '{regex}' < ./temp_files/monaa_input/log.txt > ./temp_files/monaa_output/monaa_output.txt"
+
+    result = subprocess.run(
+        ['ls', '-l'],
+        capture_output = True, 
+        text = True
+    )
+    print(result.stdout)
+    print(result.stderr)
 
     #return monaa result
     return """
