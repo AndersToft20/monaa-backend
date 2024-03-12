8ea83affc27b5cc02fb3fc053d0742de0e491ce4
unknown <Asbjoernjc@gmail.com>
2024-03-12 10:13:36 +0100

    commented some stuff out

--- monaa.py
+++ monaa.py
@@ -3,8 +3,8 @@ import os
 def monaa_handler(file, regex: str):
     path = os.path.join("./temp_files/monaa_input", "log.txt")
     file.save(path)
-    with open(path, "r") as f:
-        print(f.readlines())
+    # with open(path, "r") as f:
+    #     print(f.readlines())
 
     #call mona with input however that may be done
     command = f"../monaa/build/monaa -e '{regex}' < ./temp_files/monaa_input/log.txt > ./temp_files/monaa_output/monaa_output.txt"
