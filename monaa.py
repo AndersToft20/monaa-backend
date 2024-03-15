360ef70da256340a5c0a0349b31239cc3754a1e1
AndersToft20 <atoft20@student.aau.dk>
2024-03-15 11:35:33 +0100

    what

--- monaa.py
+++ monaa.py
@@ -1,6 +1,6 @@
 import os
 
-def monaa_handler(file: list[list[str]], regex: str):
+def monaa_handler(file: list[str], regex: str):
     path = os.path.join("./temp_files/monaa_input", "log.txt")
     with open(path, "w") as f:
         for line in file:
