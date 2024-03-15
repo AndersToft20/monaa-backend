0a953a2eea33c3b60dabc131ebe35ba38a1fef1f
AndersToft20 <atoft20@student.aau.dk>
2024-03-15 11:32:53 +0100

    save array to file

--- monaa.py
+++ monaa.py
@@ -1,9 +1,10 @@
 import os
 
-def monaa_handler(file, regex: str):
+def monaa_handler(file: list[list[str]], regex: str):
     path = os.path.join("./temp_files/monaa_input", "log.txt")
     with open(path, "w") as f:
-        f.writelines(file)
+        for line in file:
+            f.write(line + '\n')
 
     #call mona with input however that may be done
     command = f"../monaa/build/monaa -e '{regex}' < ./temp_files/monaa_input/log.txt > ./temp_files/monaa_output/monaa_output.txt"
