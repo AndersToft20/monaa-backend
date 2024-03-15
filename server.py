e515e1bb1a8ea2a4db7ec0d3fc554ea6400a6f98
AndersToft20 <atoft20@student.aau.dk>
2024-03-15 11:25:02 +0100

    request.form

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ CORS(app)
 
 @app.route('/monaa/', methods=['POST'])
 def monaa():
-    if not "file" in request.files:
+    if not "file" in request.form:
         return error_response("Missing file in request")
     if not "regex" in request.form:
         return error_response("Missing regex in request")
 
-    file = request.post['file']
+    file = request.form['file']
     print(file)
     if not file.filename.endswith('.txt'):
         return error_response("File format is incorrect. Only .txt files are allowed.")
